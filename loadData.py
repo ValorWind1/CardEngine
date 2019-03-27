2fb107555ec20e98f3d1eedd2b41a92a33fb8b78
ValorWind1 <skyjdani@gmail.com>
2019-03-26 20:16:39 -0400

    multiple cards + input

--- loadData.py
+++ loadData.py
@@ -1,12 +1,13 @@
 import pickle
 
 class UserInputs:
-    def __init__(self, dmg, hp,nm,lbl):
+    def __init__(self, dmg, hp,nm,lbl,qt):
         self.hp = hp
         self.dmg = dmg
         self.hp= hp
         self.nm = nm
         self.lbl=lbl
+        self.qt=qt
 
 def save(obj):
     save_file = open('save.dat', 'wb')
@@ -19,17 +20,32 @@ def load():
     return loaded_game_data
 
 def start():
-    UserInputs.nm= input('Card Name: ')
-    UserInputs.lbl = input('Card Label: ')
-    UserInputs.dmg = input('Card Dmg: ')
-    UserInputs.hp = input('Card Hp: ')
+    while True:
+        UserInputs.nm= input('Card Name: ')
+        UserInputs.lbl = input('Card Label: ')
+        UserInputs.dmg = input('Card Dmg: ')
+        UserInputs.hp = input('Card Hp: ')
+
+        with open("output.txt", "a") as file:
+                file.write(UserInputs.nm + "\n")
+                file.write(UserInputs.lbl + "\n")
+                file.write(UserInputs.dmg + "\n")
+                file.write(UserInputs.hp + "\n\n")
+        file.close()
+        UserInputs.qt = input("Do you want to create a new card ? y/n ")
+        if UserInputs.qt =="n":
+            break
+
+
+
+
 
 def main ():
-    player = UserInputs(0,0,0,0)
+    player = UserInputs(0,0,0,0,0)
     start()
     save(UserInputs)
     loaded_player = load()
-    print (loaded_player.hp, loaded_player.dmg,loaded_player.lbl,loaded_player.nm)
+    print (loaded_player.hp, loaded_player.dmg,loaded_player.lbl,loaded_player.nm,loaded_player.qt)
 
 if __name__ == '__main__':
     main()
